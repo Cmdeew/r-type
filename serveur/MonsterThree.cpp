f7fdd8b3e056f7845221d1a2a2543f8c1ad60747
manot_p <manot_p@localhost.localdomain>
2011-01-11 11:30:53 -0500

    lib

--- serveur/MonsterThree.cpp
+++ serveur/MonsterThree.cpp
@@ -1,13 +1,15 @@
 #include <stdlib.h>
 #include <iostream>
-#include "MonsterThree.h"
+#include "Object.h"
+#include "./inc/UMonsterThree.h"
 
-MonsterThree::MonsterThree(const char id, const char x, const char y, const char type)
+MonsterThree::MonsterThree(char id, char x, char y, char type)
 {
   m_id = id;
   m_x = x;
   m_y = y;
-  m_type = type;
+  m_type = 13;
+  m_lim = 0;
 }
 
 MonsterThree::~MonsterThree()
@@ -15,7 +17,31 @@ MonsterThree::~MonsterThree()
 
 }
 
-void MonsterThree::move()
+void MonsterThree::move(Session *s)
 {
+  Object *obj;
 
+  if (m_type == 13)
+    {
+      m_x--;
+      if (m_lim < 5)
+	m_y++;
+      else
+	m_y--;
+      if (m_lim == 10)
+	m_lim = 0;
+      else
+	m_lim++;
+    }
+}
+
+extern "C"
+{
+  Object *Create(char id, char x, char y)
+  {
+    Object *save;
+
+    save = new MonsterThree(id, x, y, 11);
+    return (save);
+  }
 }
