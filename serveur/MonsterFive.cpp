89eb9c69359aa895739559dea628d36c8b1813b2
manot_p <manot_p@localhost.localdomain>
2011-01-12 08:43:57 -0500

    nouvelle implementation des mobs

---
+++ serveur/MonsterFive.cpp
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <iostream>
+#include "Object.h"
+#include "./inc/UMonsterFive.h"
+
+MonsterFive::MonsterFive(char id, char x, char y, char type)
+{
+  m_id = id;
+  m_x = x;
+  m_y = y;
+  m_type = 13;
+  m_lim = 0;
+}
+
+MonsterFive::~MonsterFive()
+{
+
+}
+
+void MonsterFive::move(Session *s)
+{
+  Object *obj;
+
+  m_x++;
+  m_y--;
+}
+
+extern "C"
+{
+  Object *Create(char id, char x, char y)
+  {
+    Object *save;
+
+    save = new MonsterFive(id, x, y, 11);
+    return (save);
+  }
+}
---
+++ serveur/inc/UMonsterFive.h
@@ -0,0 +1,17 @@
+#ifndef _MONSTERFIVE_H_
+# define _MONSTERFIVE_H_
+
+#include "../Object.h"
+
+class	MonsterFive : public Object
+{
+ public:
+  MonsterFive(char id, char x, char y, char type = 11);
+  void	move(Session *s);
+  ~MonsterFive();
+  char getType();
+};
+
+typedef MonsterFive *(*maker_monster)(char id, char x, char y);
+
+#endif // _MONSTERFIVE_H_
