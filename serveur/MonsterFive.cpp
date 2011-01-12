92c0eb1982782c679b7b8c9308068c47f74ac232
ludo <ludo@ludo.fr>
2011-01-12 11:25:54 +0100

    ajout de la 5 eme lib

---
+++ serveur/MonsterFive.cpp
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <iostream>
+#include "Object.h"
+#include "./inc/UMonsterFive.h"
+
+MonsterFive::MonsterFive(char id, char x, char y, char type)
+{
+  m_id = id;
+  m_x = x;
+  m_y = y;
+  m_type = 15;
+  m_lim = 0;
+}
+
+MonsterFive::~MonsterFive()
+{
+
+}
+
+extern "C"
+{
+  Object *Create(char id, char x, char y)
+  {
+    Object *save;
+
+    save = new MonsterFive(id, x, y, 11);
+    return (save);
+  }
+}
